a0fd72e70be4163a6463922f6a6cbd099f082c6d
Christopher Hannelas <101250058+christopherhannelas@users.noreply.github.com>
2022-06-09 14:17:31 +0400

    footer new update

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,19 +3,22 @@ import styled from 'styled-components';
 
 export const footerContainer = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   position: fixed;
   left: 0;
   bottom: 0;
   color: black;
   text-align: center;
-  font-size: 200;
+  font-size: small;
   margin: 5rem auto 0 auto;
 `;
 
 export default function Footer() {
   return (
     <footerContainer>
-      <p>Project build by Christopher Hannelas - 2022</p>
+      <p>🚀 Project build by Christopher Hannelas - 2022</p>
       <p>
         <a href='mailto:christopherhannelas@gmail.com'>🖃 Contact me</a>
       </p>
